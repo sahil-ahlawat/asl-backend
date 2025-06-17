617600df2412c3570e4ceb62efbbcea0dfadee08
Sahil Ahlawat <sahilahlawat2@gmail.com>
2025-06-17 17:19:50 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,16 @@
+// main.go
 package main
 
 import (
-	"database/sql"
-	"log"
-
 	"github.com/gofiber/fiber/v2"
-	_ "github.com/go-sql-driver/mysql"
-
-	"fitness/config"
-	"fitness/routes"
 )
+
 func main() {
-	config.LoadEnv()
 	app := fiber.New()
 
-	db, err := sql.Open("mysql", "root:root@tcp(host.docker.internal:3306)/fitness") // Change credentials as needed
-	if err != nil {
-		log.Fatal(err)
-	}
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World from new fiber !")
+	})
 
-	routes.SetupListingRoutes(app, db)
-
-	port := config.GetEnv("APP_PORT", "3000")
-	log.Fatal(app.Listen(":" + port))
+	app.Listen(":3000")
 }
